8359f3a206ebf6fed4b9d65f9942898a1f89cc39
danpatrickoneill <dan47573@gmail.com>
2019-04-30 16:15:21 -0400

    Added path not found exception

--- dogs-initial/src/main/java/com/lambdaschool/projectrestdogs/handler/RestExceptionHandler.java
+++ dogs-initial/src/main/java/com/lambdaschool/projectrestdogs/handler/RestExceptionHandler.java
@@ -4,10 +4,13 @@ import com.lambdaschool.projectrestdogs.exceptions.ResourceNotFoundException;
 import com.lambdaschool.projectrestdogs.model.ErrorDetail;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.NoHandlerFoundException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import javax.servlet.http.HttpServletRequest;
@@ -26,6 +29,18 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
         errorDetail.setTitle("Resource not found");
         errorDetail.setDetail(exception.getMessage());
         errorDetail.setDeveloperMessage(exception.getClass().getName());
+
+        return new ResponseEntity<>(errorDetail, null, HttpStatus.NOT_FOUND);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleNoHandlerFoundException(NoHandlerFoundException exception, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        ErrorDetail errorDetail = new ErrorDetail();
+        errorDetail.setTimestamp(new Date().getTime());
+        errorDetail.setStatus(HttpStatus.NOT_FOUND.value());
+        errorDetail.setTitle(exception.getRequestURL());
+        errorDetail.setDetail(request.getDescription(true));
+        errorDetail.setDeveloperMessage("Rest Handler Not Found (check for valid URI)");
 
         return new ResponseEntity<>(errorDetail, null, HttpStatus.NOT_FOUND);
     }
