d6d0b6f9ea75ce82b49cade6de428997f494523d
danpatrickoneill <dan47573@gmail.com>
2019-05-01 16:04:58 -0400

    Added logging on access to each endpoint

--- dogs-initial/src/main/java/com/lambdaschool/projectrestdogs/controller/DogController.java
+++ dogs-initial/src/main/java/com/lambdaschool/projectrestdogs/controller/DogController.java
@@ -3,6 +3,8 @@ package com.lambdaschool.projectrestdogs.controller;
 import com.lambdaschool.projectrestdogs.exceptions.ResourceNotFoundException;
 import com.lambdaschool.projectrestdogs.model.Dog;
 import com.lambdaschool.projectrestdogs.ProjectrestdogsApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.TypeMismatchException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,10 +19,13 @@ import java.util.ArrayList;
 @RequestMapping("/dogs")
 public class DogController
 {
+    private static final Logger logger = LoggerFactory.getLogger(DogController.class);
+
     // localhost:8080/dogs/dogs
     @GetMapping(value = "/dogs")
     public ResponseEntity<?> getAllDogs()
     {
+        logger.info("/dogs/dogs accessed.");
         return new ResponseEntity<>(ProjectrestdogsApplication.ourDogList.dogList, HttpStatus.OK);
     }
 
@@ -35,7 +40,9 @@ public class DogController
         }
 
         rtnDog = ProjectrestdogsApplication.ourDogList.findDog(d -> (d.getId() == id));
+        logger.info("/dogs/{} accessed.", id);
         return new ResponseEntity<>(rtnDog, HttpStatus.OK);
+
     }
 
     // localhost:8080/dogs/breeds/{breed}
@@ -49,6 +56,7 @@ public class DogController
             throw new ResourceNotFoundException("No dogs of breed " + breed + " found.");
         }
 
+        logger.info("/dogs/breeds/{} accessed.", breed);
         return new ResponseEntity<>(rtnDogs, HttpStatus.OK);
     }
 }
